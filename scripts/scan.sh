8c30301cd09cf5318856f76be888cbdcd1615b76
zsolomon <zev.solomon@sony.com>
2024-08-21 18:18:27 +0900

    K: again

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -8,3 +8,4 @@ echo "\n========= SCANNING FOR PHONE NUMBERS ========="
 grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
 grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
 echo "========= PHONE NUMBER SCAN COMPLETE ========="
+
